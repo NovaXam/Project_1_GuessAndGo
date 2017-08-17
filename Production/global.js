602bd30f958771ee00f1b40dc3f2afe124fb0dbf
Maksym Novakh <novakhm@gmail.com>
2017-08-17 11:28:23 -0400

    refactor_part1

--- Production/global.js
+++ Production/global.js
@@ -1,16 +1,32 @@
+  function gameTimer() {
+   function timeChecker() {
+      if (stepFlag == 12) {
+          clearInterval(inTimer);
+          $('#level').text(`00 : 0${sec}`);
+          };
 
-
-//   EventListerner for start/reset button.
-                       // When it clicked create a new Object(newGame) of class Game with parameters array of pictures and empty array.
-//                     Call the method launch the newGame.start() with triggers the game flow;
-//                     Change the value of Start button for Reset button and id from "start" to "reset";
-//   timer(time) - stop the program flow for "time" second. Argument gets from parent method which calls it.
-//   launchTimer(num) - create modal window and shows on it digits from time to 0 with interval of 1 second. To manage the
-//                      time use timer method inside of the loop. Return true. As an argument accepts number(time in seconds).
-//
-
-$('button').on('click', () => {
-    var newGame = new Class(arrayOfTopics, arrayOfPictures);
-    newGame.start(isStartOrReset);
-    $('<button>').attr('id','reset');
-});
+      if (progressFlag < 8 && sec >= 10) {
+          $('#level').text(`00 : ${sec}`);
+          sec--;
+          } else if (progressFlag < 8 && sec < 10 && sec > 0) {
+          $('#level').text(`00 : 0${sec}`);
+          sec--;
+          } else if (progressFlag == 8) {
+          $('#level').text(`00 : ${sec}`);
+          clearInterval(inTimer);
+          } else if (sec == 0) {
+          $('#level').text(`00 : 0${sec}`);
+          clearInterval(inTimer);
+          $('.Winner_Looser').css('visibility', 'visible');
+          $('.Winner_Looser').children('span').remove();
+          $('.wl').css('display', 'inline-block');
+          $('#stat').text(`steps ${stepFlag} : progress ${progressFlag} score ${score}`);
+          $('#cup').css('background-image', looserCup);
+          $('#cup').css("background-size", '60%');
+          $('#cup').css("background-repeat", 'no-repeat');
+          $('#cup').css("background-position", 'center');
+          $('#text').text('TRY HARDER');
+          }
+        };
+  let inTimer = setInterval(function() {timeChecker(sec)}, 1000);
+}
